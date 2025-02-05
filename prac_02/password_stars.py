703251f89c1d44cb159b855f8bba61727db74cb5
eight <junyi.qi@my.jcu.edu.au>
2025-02-05 23:52:15 +0800

    Refactoring password checking program to use functions and adding document string comments.

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,11 @@
+"""This module implements a password checking program.
+It prompts the user to enter a password to verify if it meets the minimum length requirement,
+and then display a series of asterisks corresponding to the password length."""
+
 PASSWORD_LENGTH = 10
+
 def main():
+    """Obtain password, verify length, and display corresponding asterisk."""
     password = get_password()
     while len(password) < PASSWORD_LENGTH:
         print(f'Password must be at least {PASSWORD_LENGTH} characters long.')
@@ -7,9 +13,11 @@ def main():
     print_password(password)
 
 def print_password(password):
+    """Display asterisks indicating the corresponding number of passwords."""
     print("*" * len(password))
 
 def get_password():
+    """Retrieve the password entered by the user."""
     password = input(f'Enter a password (at least {PASSWORD_LENGTH} characters): ')
     return password
 
