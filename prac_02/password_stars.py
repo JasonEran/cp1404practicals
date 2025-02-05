746421e76638fd51eab5486a2464c85cf6f4b025
eight <junyi.qi@my.jcu.edu.au>
2025-02-05 14:55:56 +0800

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,9 @@
 PASSWORD_LENGTH = 10
-password = input('Enter a password: ')
-while len(password) < PASSWORD_LENGTH:
-    print(f'Password must be at least {PASSWORD_LENGTH} characters long.')
-    password = input('Enter a password: ')
-print("*" * len(password))
+def main():
+    password = input(f'Enter a password (at least {PASSWORD_LENGTH} characters): ')
+    while len(password) < PASSWORD_LENGTH:
+        print(f'Password must be at least {PASSWORD_LENGTH} characters long.')
+        password = input('Enter a password: ')
+    print("*" * len(password))
+
+main()
