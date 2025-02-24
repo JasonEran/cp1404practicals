0049f419a3389f7661ee3a1e9c8d7df6ad124669
eight <junyi.qi@my.jcu.edu.au>
2025-02-24 16:13:16 +0800

    Update to new branch

---
+++ prac_05/email.py
@@ -0,0 +1,36 @@
+"""
+Emails
+Estimate: 20 minutes
+Actual:   36 minutes
+"""
+
+def main():
+    """Store users' emails and names in a dictionary."""
+    email_to_name = {}
+    email = input("Email: ").strip()
+
+    while email:
+
+        name = extract_name(email)
+        correct_name = input(f"Is your name {name}? (Y/n): ").strip().lower()
+
+        if correct_name not in ['n', 'no']:
+            email_to_name[email] = name
+        else:
+            name = input("Name: ").strip()
+            email_to_name[email] = name
+
+        email = input("Email: ").strip()
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def extract_name(email):
+    """Extract name from email address."""
+    local_part = email.split('@')[0]
+    name_parts = local_part.split('.')
+    name = ' '.join(name_parts).title()
+    return name
+
+main()
---
+++ prac_05/wimbledon.py
@@ -0,0 +1,7 @@
+"""
+Wimbledon
+Estimate: 60 minutes
+Actual:    minutes
+"""
+def main():
+    print("Wimbledon Champions:")
