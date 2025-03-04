165fef71b0f7dae3910ba5bcd4335b895b0bed57
eight <junyi.qi@my.jcu.edu.au>
2025-03-04 15:14:23 +0800

    Complete the coding of the ProgramingLanguage class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,5 +1,23 @@
 """
 Programming Languages
 Estimate: 20 minutes
-Actual:    minutes
+Actual:   10 minutes
 """
+
+class ProgrammingLanguage:
+    """Represent a programming language object."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a programming language instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Returns the formatted string representation of the programming language."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Return True if the language has dynamic typing, otherwise False."""
+        return self.typing.lower() == "dynamic"
