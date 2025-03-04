b4f46aa40c1b19c84369d39b4bddeac5a75c9823
eight <junyi.qi@my.jcu.edu.au>
2025-03-04 22:50:35 +0800

    Complete preliminary code writing

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,6 +1,33 @@
 """
 Guitars
 Estimate: 20 minutes
-Actual:    minutes
+Actual:   12 minutes
 """
 
+from prac_06.guitar import Guitar
+
+def main():
+    """Store all users' guitars and print their detailed information."""
+    print("My guitars!")
+    guitars = []
+    name = input("Name: ")
+    # User input loop
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.\n")
+        name = input("Name: ")
+    # Test data
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    # Display guitars
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+main()
