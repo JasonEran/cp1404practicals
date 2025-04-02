1eca0b4aa6a5e8a9da450989eafe63f48b1f2260
eight <junyi.qi@my.jcu.edu.au>
2025-04-02 16:41:12 +0800

    Modify test structure

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -4,15 +4,20 @@ taxi test
 """
 from taxi import Taxi
 
-my_taxi = Taxi("Prius 1", 100)
+def main():
+    """Test taxi class"""
+    my_taxi = Taxi("Prius 1", 100)
 
-my_taxi.drive(40)
+    my_taxi.drive(40)
 
-print(my_taxi)
-print(f"Current fare: ${my_taxi.get_fare():.2f}")
+    print(my_taxi)
+    print(f"Current fare: ${my_taxi.get_fare():.2f}")
 
-my_taxi.start_fare()
-my_taxi.drive(100)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
 
-print(my_taxi)
-print(f"Current fare: ${my_taxi.get_fare():.2f}")
+    print(my_taxi)
+    print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
+if __name__ == "__main__":
+    main()
